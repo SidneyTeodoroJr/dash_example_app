013a4a18cfb65eb992b42f4e7e3076684706ee58
Sidney Teodoro Araujo Junior <91035485+SidneyTeodoroJr@users.noreply.github.com>
2023-04-23 13:54:48 -0300

    Add files via upload

---
+++ dash_example.py
@@ -0,0 +1,29 @@
+from dash import Dash, html, dcc
+import plotly.express as px
+import pandas as pd
+
+app = Dash(__name__)
+
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
+
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello!! Welcome!'),
+
+    html.Div(children='''
+        Simple interactive chart example with Dash and Python
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
